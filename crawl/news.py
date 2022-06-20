59aa516d39f813520bf9f0773e69f9ff45c5f5e0
G Brezeanu <gbrezeanu@protonmail.com>
2022-06-21 01:08:38 +0300

    scrapy

---
+++ crawl/news.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class Quanta(scrapy.Spider):
+    name = ''
+    start_urls = ['']
+
+    def parse(self, response):
+        for title in response.css('.oxy-post-title'):
+            yield {'title': title.css('::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
